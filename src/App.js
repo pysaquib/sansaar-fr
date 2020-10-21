82966bd6ff9dc40a420b97cb9145ccd0e507466e
komalahire <komala18@navgurukul.org>
2020-10-21 14:06:49 +0530

    login/signup page through google authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Login from "./views/Login/login";
 import { Grid } from "@material-ui/core";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./Router/history"
+import HomePage from './components/HomePage'
+
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/Home" component={HomePage} />
+
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
--- src/views/Login/login.js
+++ src/views/Login/login.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import LockOpenTwoToneIcon from "@material-ui/icons/LockOpenTwoTone";
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Grid,
-  Typography,
-} from "@material-ui/core";
+import {Button,Card,CardActions,CardContent,Grid,Typography,} from "@material-ui/core";
+import GoogleLogin from 'react-google-login';
 import useStyles from "./styles";
+import axios from 'axios';
+import {Link} from 'react-router-dom'
 
-function Login() {
+function Login(props) {
+const responseGoogle = response => {
+    console.log(response.tokenObj.id_token);
+
+    const body = { idToken: response.tokenObj.id_token, mode: 'web' };
+    axios
+      .post("http://dev-api.navgurukul.org/apiDocs/users/auth/google", body)
+      .then((res) => {
+        console.log(res, "respones")
+        props.history.push('/Home')
+      })
+
+      .catch((err) => {
+        console.log(err, "errorr");
+
+      });
+
+
+  }
   const classes = useStyles();
   const [isLogin, setLoginType] = useState(false);
   let message = (
@@ -35,10 +49,41 @@ function Login() {
           </CardContent>
           <CardActions className={classes.cardContent}>
             <Button className={classes.button}>
-              <Typography variant="button" className={classes.loginText}>
-                {isLogin ? "Login" : "Let's get started"}
-              </Typography>
+              {isLogin ? <GoogleLogin
+
+                clientId="34917283366-b806koktimo2pod1cjas8kn2lcpn7bse.apps.googleusercontent.com"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                render={(renderProps) => (
+                  <Button
+                    onClick={renderProps.onClick}
+                    className={classes.buttons}
+                    variant="contained"
+                    size="large"
+
+                  >
+                    Login with Google
+                  </Button>
+                )}
+
+              /> : <GoogleLogin
+                  clientId="34917283366-b806koktimo2pod1cjas8kn2lcpn7bse.apps.googleusercontent.com"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  render={(renderProps) => (
+                    <Button
+                      onClick={renderProps.onClick}
+                      variant="contained"
+                      className={classes.buttons}
+                      size="large"
+                      color="primary"
+                      >
+                      Signup with Google
+                    </Button>
+                  )}
+                />}
             </Button>
+
             <Typography className={classes.swap}>{message}</Typography>
           </CardActions>
         </Card>
@@ -48,3 +93,25 @@ function Login() {
 }
 
 export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
